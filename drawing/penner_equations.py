f2238a29f7e8c072f4007a9221a773898f3f75f7
Johan Borgström <johan@petfactory.se>
2015-08-15 20:08:42 +0200

    lerp min/max

--- drawing/penner_equations.py
+++ drawing/penner_equations.py
@@ -8,7 +8,7 @@ def lerp(x0, y0, x1, y1, x):
 #t: current time, b: begInnIng value, c: change In value, d: duration
 
 def linear(t, b, c, d):
-    return (c-b)*(t/d)
+    return c*t+b
 
 def easeInCubic(t, b, c, d):
     t /= d
@@ -52,11 +52,34 @@ class Widget(QtGui.QWidget):
         vbox.addWidget(self.combo)
         self.combo.currentIndexChanged.connect(self.index_changed)
 
+        self.min_spinbox = QtGui.QDoubleSpinBox()
+        self.min_spinbox.setSingleStep(.05)
+        self.min_spinbox.setRange(0, 0.95)
+        vbox.addWidget(self.min_spinbox)
+
+        self.min_spinbox.valueChanged.connect(self.min_spinbox_change)
+
+
+        self.max_spinbox = QtGui.QDoubleSpinBox()
+        self.max_spinbox.setSingleStep(.05)
+        self.max_spinbox.setRange(0.05, 1.0)
+        self.max_spinbox.setValue(1.0)
+
+        vbox.addWidget(self.max_spinbox)
+
+        self.max_spinbox.valueChanged.connect(self.max_spinbox_change)
+
     def index_changed(self, index):
         self.curve_canvas.change_equation(self.equations.get(self.combo.currentText()))
         self.curve_canvas.repaint()
 
-        
+    def min_spinbox_change(self, val):
+        self.curve_canvas.change_min(val)
+        self.curve_canvas.repaint()
+
+    def max_spinbox_change(self, val):
+        self.curve_canvas.change_max(val)
+        self.curve_canvas.repaint()
 
 
 
@@ -66,10 +89,8 @@ class CurveCanvas(QtGui.QWidget):
         super(CurveCanvas, self).__init__()
         
         self.equation = equation
-        #self.setGeometry(50, 50, 100, 100)
-        self.setWindowTitle('Points')
-        self.show()
-        
+        self.min = 0.0
+        self.max = 1.0
 
     def paintEvent(self, e):
 
@@ -81,43 +102,65 @@ class CurveCanvas(QtGui.QWidget):
     def drawPoints(self, qp):
       
         #qp.setPen(QtCore.Qt.red)
+        width, height = self.size().toTuple()
+        offset = 6
 
         pen = QtGui.QPen()  # creates a default pen
-        pen.setWidth(2)
+        pen.setColor(QtGui.QColor(210,210,210))
+        pen.setWidth(1)
         qp.setPen(pen)
+
+        num = 11
+        inc = 1.0/(num-1)
+        for i in range(num):
+            x = i*inc*(width-offset*2)+offset
+            y = i*inc*(height-offset*2)+offset
+            qp.drawLine(x,offset, x, height-offset)
+            qp.drawLine(offset, y, width-offset, y)
         
         
         if self.equation is None:
             return
 
-        width, height = self.size().toTuple()
-        num_p = 20;
-        inc = 1.0/(num_p-1)
+        old_min = self.min
+        old_max = self.max
 
-        '''
-        for i in range(num_p-1):
+        pen.setColor(QtGui.QColor(150,150,150))
+        qp.setPen(pen)
+
+        for i in range(num-1):
 
             u1 = i * inc
             u2 = (i+1) * inc
-            x1 = u1 * width
-            x2 = u2 * width
-            y1 = self.equation(u1, 0, height, 1.0)
-            y2 = self.equation(u2, 0, height, 1.0)
+            x1 = u1 * (width-offset*2) + offset
+            x2 = u2 * (width-offset*2) + offset
+
+            uy1 = min(1.0, max(0, lerp(old_min,0,old_max,1,u1)))
+            uy2 = min(1.0, max(0, lerp(old_min,0,old_max,1,u2)))
+            y1 = self.equation(uy1, 0, 1.0, 1.0) * (height-offset*2) + offset
+            y2 = self.equation(uy2, 0, 1.0, 1.0) * (height-offset*2) + offset
             qp.drawLine(x1, -y1+height, x2, -y2+height)
-        '''
-        offset = 6
-        pen.setWidth(5)
+
+
+        pen.setWidth(4)
+        pen.setColor(QtCore.Qt.black)
         qp.setPen(pen)
 
-        for i in range(num_p):
+        for i in range(num):
             u = i * inc
+            uy = min(1.0,max(0, lerp(old_min,0,old_max,1,u)))
             x = u * (width-offset*2) + offset
-            y = self.equation(u, 0, (height-offset*2), 1.0)
-            qp.drawPoint(x, -(y+offset)+height)
-            print(x, y+ offset)
+            y = self.equation(uy, 0, 1.0, 1.0) * (height-offset*2) + offset
+            qp.drawPoint(x, -y+height)
 
     def change_equation(self, equation):
         self.equation = equation
+
+    def change_min(self, val):
+        self.min = val
+
+    def change_max(self, val):
+        self.max = val
 
 def main():
 
