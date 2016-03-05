2de98a57d4255f66b94e222150aefcb0566cd072
Johan Borgström <johan@petfactory.se>
2016-03-05 12:52:14 +0100

    add popup menu

--- polygon_hoover/polygon_hoover.py
+++ polygon_hoover/polygon_hoover.py
@@ -2,54 +2,66 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from functools import partial
 from PySide import QtGui, QtCore
+
+def apa(name):
+    print name
 
 class MyPathItem(QtGui.QGraphicsPathItem):
 
-    def __init__(self, name, scene):
+    def __init__(self, name, quick_groups=None):
         super(MyPathItem, self).__init__()
         self.setAcceptHoverEvents(True)
         self.setOpacity(.2)
         self.name = name
-        self.scene = scene
-        self.quick_button = None
+        self.quick_groups = quick_groups
 
     def hoverEnterEvent(self, event):
-        print('Enter: {}').format(self.name)
-        self.setCursor(QtCore.Qt.PointingHandCursor)
+        #print('Enter: {}').format(self.name)
+
+        if self.quick_groups:
+            self.setCursor(QtCore.Qt.CrossCursor)
+        else:
+            self.setCursor(QtCore.Qt.PointingHandCursor)
+
         self.setOpacity(.6)
 
     def hoverLeaveEvent(self, event):
-        print('Leave: {}').format(self.name)
+        #print('Leave: {}').format(self.name)
         self.setOpacity(.2)
 
     def mousePressEvent(self, event):
 
-        if self.quick_button:
-            self.quick_button.deleteLater()
-            self.quick_button = None
-
         modifiers = QtGui.QApplication.keyboardModifiers()
-        if modifiers == QtCore.Qt.ShiftModifier:
-            print('Shift+Click')
 
-            self.quick_button = QtGui.QPushButton('Test')
-            x, y = event.scenePos().toTuple()
-            self.quick_button.move(x, y)
-            self.scene.addWidget(self.quick_button)
-
-        else:    
+        if modifiers == QtCore.Qt.NoModifier:
             print self.name
 
-    def mouseReleaseEvent(self, event):
-
-        if self.quick_button:
-            self.quick_button.deleteLater()
-            self.quick_button = None
+        else:
+            print 'Modifier press' 
+            
 
     def shape(self):
         return self.path()
 
+    def contextMenuEvent(self, event):
+
+        if self.quick_groups:
+
+            menu = QtGui.QMenu()
+
+            for name in self.quick_groups:
+                action = menu.addAction(name)
+                action.triggered.connect(partial(apa, name))
+
+            selectedAction = menu.exec_(event.screenPos())
+        else:
+            print('"{}" has no quick groups').format(self.name)
+
+
+    def add_quick_groups(self, quick_groups=None):
+        self.quick_groups = quick_groups
 
 class BaseWin(QtGui.QWidget):
     
@@ -66,18 +78,21 @@ class BaseWin(QtGui.QWidget):
     
         self.scene.addPixmap(QtGui.QPixmap('mario.png'))
 
-        self.add_hit_path(  outer_pos_list=[(10,10), (90,10), (40,50), (10,50)],
-                            name='Mario',
-                            color=QtGui.QColor(255, 0, 0, 255),
-                            scene=self.scene,
-                            inner_pos_list=[(20,20), (60,20), (30,40), (20,40)])
+        mario_item = self.add_hit_path( outer_pos_list=[(10,10), (90,10), (40,50), (10,50)],
+                                        name='Mario',
+                                        color=QtGui.QColor(255, 0, 0, 255),
+                                        scene=self.scene,
+                                        quick_groups=['Three', 'Four', 'Five'],
+                                        inner_pos_list=[(20,20), (60,20), (30,40), (20,40)])
 
 
-        self.add_hit_path(  outer_pos_list=[(100,100), (200,100), (200,200), (100,200)],
-                            name='Toad',
-                            color=QtGui.QColor(0, 255, 135, 255),
-                            scene=self.scene)
+        toad_item = self.add_hit_path(  outer_pos_list=[(100,100), (200,100), (200,200), (100,200)],
+                                        name='Toad',
+                                        color=QtGui.QColor(0, 255, 135, 255),
+                                        scene=self.scene,
+                                        quick_groups=['One', 'Two'])
 
+        toad_item.add_quick_groups()
 
 
         view = QtGui.QGraphicsView(self.scene)
@@ -85,7 +100,7 @@ class BaseWin(QtGui.QWidget):
         
         vbox.addWidget(view)
 
-    def add_hit_path(self, outer_pos_list, name, color, scene, inner_pos_list=None):
+    def add_hit_path(self, outer_pos_list, name, color, scene, quick_groups=None, inner_pos_list=None):
 
         path = QtGui.QPainterPath()
 
@@ -96,12 +111,14 @@ class BaseWin(QtGui.QWidget):
             polygon = [QtCore.QPoint(*p) for p in inner_pos_list]
             path.addPolygon(polygon)
  
-        item = MyPathItem(name, scene)
+        item = MyPathItem(name, quick_groups)
         item.setPath(path)
         item.setBrush(QtGui.QBrush(color))
         item.setPen(QtGui.QPen(QtCore.Qt.NoPen))  
 
         scene.addItem(item)
+
+        return item
 
 def main():
     
