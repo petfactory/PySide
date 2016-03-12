61f4e56308099373ae914f4d03b6388ca83ebb6f
Johan Borgström <johan@petfactory.se>
2016-03-12 11:01:02 +0100

    add data to spec file

--- png_comp/png_comp.py
+++ png_comp/png_comp.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, os
 from PySide import QtGui, QtCore
 
 
@@ -14,34 +14,44 @@ class BaseWin(QtGui.QWidget):
         self.setWindowTitle('Test')
 
         self.image_dict = {}
-        self.image_dict['blue'] = QtGui.QGraphicsPixmapItem(QtGui.QPixmap('blue.png'))
-        self.image_dict['green'] = QtGui.QGraphicsPixmapItem(QtGui.QPixmap('green.png'))
-        self.image_dict['red'] = QtGui.QGraphicsPixmapItem(QtGui.QPixmap('red.png'))
+        self.image_dict['blue'] = QtGui.QGraphicsPixmapItem(QtGui.QPixmap(self.resource_path('blue.png')))
+        self.image_dict['green'] = QtGui.QGraphicsPixmapItem(QtGui.QPixmap(self.resource_path('green.png')))
+        self.image_dict['red'] = QtGui.QGraphicsPixmapItem(QtGui.QPixmap(self.resource_path('red.png')))
 
         self.scene = QtGui.QGraphicsScene()
     
-        self.model = QtGui.QStandardItemModel()    
+        self.model = QtGui.QStandardItemModel()
+        self.model.itemChanged.connect(self.item_changed)
 
         self.tableview = QtGui.QTableView()
         header = self.tableview.horizontalHeader()
         header.setStretchLastSection(True)
         #self.tableview.setAlternatingRowColors(True)
         self.tableview.setModel(self.model)
-        self.tableview.clicked.connect(self.tableview_clicked)
+        #self.tableview.clicked.connect(self.tableview_clicked)
         self.add_items()
         self.model.setHorizontalHeaderLabels(['Layers'])
 
         view = QtGui.QGraphicsView(self.scene)
-        #updateSceneRect
         view.setSceneRect(0,0,800,600)        
 
-        splitter =  QtGui.QSplitter(self)
+        splitter = QtGui.QSplitter(self)
         splitter.addWidget(self.tableview)
         splitter.addWidget(view)
 
-    def tableview_clicked(self, index):
-        z_value = index.row()
-        item = self.model.itemFromIndex(index)
+    def resource_path(self, relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        try:
+            # PyInstaller creates a temp folder and stores path in _MEIPASS
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+
+        return os.path.join(base_path, relative_path)
+
+    def item_changed(self, item):
+
+        z_value = item.row()
         pixmapItem = self.image_dict.get(item.text())
         if not pixmapItem:
             return
@@ -53,8 +63,9 @@ class BaseWin(QtGui.QWidget):
             self.scene.removeItem(pixmapItem)
 
 
-
     def add_items(self):
+
+        self.model.blockSignals(True)
 
         name_list = ['blue', 'green', 'red']
 
@@ -64,6 +75,8 @@ class BaseWin(QtGui.QWidget):
             
             item.setCheckable(True)
             self.model.setItem(row, 0, item)
+
+        self.model.blockSignals(False)
 
 
 
