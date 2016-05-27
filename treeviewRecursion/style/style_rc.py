e2e116b282a6bf12f735ba236cefde0a4431c22c
Johan Borgström <johan@petfactory.se>
2016-05-27 09:45:51 +0200

    rebuild rc

--- treeviewRecursion/style/style_rc.py
+++ treeviewRecursion/style/style_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Thu May 26 23:46:12 2016
+# Created: Fri May 27 09:39:09 2016
 #      by: The Resource Compiler for PySide (Qt v4.8.7)
 #
 # WARNING! All changes made in this file will be lost!
