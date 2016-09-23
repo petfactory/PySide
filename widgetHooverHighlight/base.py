3f931f0b92ad16b0a3288131a1339a2a38e58173
Johan Borgström <johan@petfactory.se>
2016-09-23 16:21:24 +0200

    add ui to turntablerig

--- widgetHooverHighlight/base.py
+++ widgetHooverHighlight/base.py
@@ -25,30 +25,60 @@ class BaseWin(QtGui.QWidget):
     def __init__(self):
         super(BaseWin, self).__init__() 
 
-        self.setGeometry(50, 100, 300, 500)
+        self.setGeometry(50, 100, 300, 300)
         self.setWindowTitle('Test')
         
         self.vbox = QtGui.QVBoxLayout(self)
 
         self.scene = QtGui.QGraphicsScene()
-        self.scene.addPixmap(QtGui.QPixmap('marioBW.png'))
-
+        self.scene.addPixmap(QtGui.QPixmap('base.png'))
         view = QtGui.QGraphicsView(self.scene)
-        view.setSceneRect(0,0 , 400, 400)
+        self.vbox.setContentsMargins(0, 0, 0, 0)
+        view.setSceneRect(0, 0, 300, 240)
+        view.setFixedSize(302, 242)
         self.vbox.addWidget(view)
 
-        self.camDistLabel, self.camDistSpinbox, self.camDistGraphic = self.addSpinBox('Camera dist', 'arrow.png', (100, 100))
-        self.camPitchLabel, self.camPitchSpinbox, self.camPitchGraphic  = self.addSpinBox('Camera pitch', 'arrow.png', (100, 200))
-        self.camJawLabel, self.camJawSpinbox, self.camJawGraphic  = self.addSpinBox('Camera jaw', 'arrow.png', (100, 300))
+        self.uiVbox = QtGui.QVBoxLayout()
+        self.vbox.addLayout(self.uiVbox)
+        self.uiVbox.setContentsMargins(6, 6, 6, 6)
 
+        self.origoLabel, self.origoSpinbox, self.origoGraphic = self.addSpinBox('Origo', 'arrow.png', (0, 0))
+        self.camDistLabel, self.camDistSpinbox, self.camDistGraphic = self.addSpinBox('Camera dist', 'arrow.png', (0, 0))
+        self.camPitchLabel, self.camPitchSpinbox, self.camPitchGraphic  = self.addSpinBox('Camera pitch', 'arrow.png', (0, 50))
+        self.camJawLabel, self.camJawSpinbox, self.camJawGraphic  = self.addSpinBox('Camera jaw', 'arrow.png', (0, 100))
+        self.focallength, self.focallengthSpinbox, self.focallengthGraphic  = self.addSpinBox('Focallength', 'arrow.png', (0, 150))
+
+        #self.useExistingCamCB = QtGui.QCheckBox('Use Existing Camera')
+        self.useExistingCamGroup = QtGui.QGroupBox('Use Existing Camera')
+        self.useExistingCamGroup.setLayout(QtGui.QVBoxLayout())
+        self.existingCameraName = QtGui.QLineEdit()
+        self.useExistingCamGroup.layout().addWidget(self.existingCameraName)
+        self.useExistingCamGroup.setCheckable(True)
+        self.uiVbox.addWidget(self.useExistingCamGroup )
+
+        self.changeFocallengthGroup = QtGui.QGroupBox('Change Focallength')
+        self.changeFocallengthGroup.setLayout(QtGui.QVBoxLayout())
+        self.newFocallengthSpinbox = QtGui.QDoubleSpinBox()
+        self.changeFocallengthGroup.layout().addWidget(self.newFocallengthSpinbox)
+        self.changeFocallengthGroup.setCheckable(True)
+        self.uiVbox.addWidget(self.changeFocallengthGroup )
+
+        createAnimCB = QtGui.QCheckBox('Animate Turntable')
+        self.uiVbox.addWidget(createAnimCB)
+
+        self.vbox.addStretch()
         
+        createButton = QtGui.QPushButton('Create')
+        self.uiVbox.addWidget(createButton)
+
 
     def addSpinBox(self, name, graphicName, graphicPos):
 
         hbox = QtGui.QHBoxLayout()
-        self.vbox.addLayout(hbox)
+        self.uiVbox.addLayout(hbox)
 
         label = QtGui.QLabel(name)
+        label.setFixedWidth(80)
         label.installEventFilter(self)
         label.setMouseTracking(True)
         hbox.addWidget(label)
